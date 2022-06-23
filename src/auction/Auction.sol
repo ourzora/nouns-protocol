6e9f72b9229fd9eee2fbde730191b8384ff279a9
Rohan Kulkarni <rohan@zora.co>
2022-06-23 06:15:12 -0400

    refactor: auction transfers

--- src/auction/Auction.sol
+++ src/auction/Auction.sol
@@ -7,17 +7,17 @@ import {ReentrancyGuardUpgradeable} from "@openzeppelin/contracts-upgradeable/se
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-import {IAuctionHouse} from "./IAuctionHouse.sol";
+import {IAuction} from "./IAuction.sol";
 import {IToken} from "../token/IToken.sol";
 import {IWETH} from "../token/external/IWETH.sol";
 
-import {IUpgradeManager} from "../upgrades/IUpgradeManager.sol";
-import {AuctionHouseStorageV1} from "./storage/AuctionHouseStorageV1.sol";
+import {IUpgradeManager} from "../upgrade/IUpgradeManager.sol";
+import {AuctionStorageV1} from "./storage/AuctionStorageV1.sol";
 
-/// @title Nounish Auction House
+/// @title Nounish Auction
 /// @author Rohan Kulkarni
-/// @notice Modified version of NounsAuctionHouse.sol (commit 2cbe6c7) that NounsDAO licensed under the GPL-3.0 license
-contract AuctionHouse is AuctionHouseStorageV1, UUPSUpgradeable, PausableUpgradeable, ReentrancyGuardUpgradeable, OwnableUpgradeable {
+/// @notice Modified version of NounsAuctionHouse.sol (commit 2cbe6c7) that Nouns licensed under the GPL-3.0 license
+contract Auction is AuctionStorageV1, UUPSUpgradeable, PausableUpgradeable, ReentrancyGuardUpgradeable, OwnableUpgradeable {
     ///                                                          ///
     ///                          IMMUTABLES                      ///
     ///                                                          ///
@@ -25,46 +25,56 @@ contract AuctionHouse is AuctionHouseStorageV1, UUPSUpgradeable, PausableUpgrade
     /// @notice The contract upgrade manager
     IUpgradeManager private immutable UpgradeManager;
 
+    /// @notice The Nouns DAO Treasury
+    address private immutable Nouns;
+
+    /// @notice The Nouns Builder DAO
+    address private immutable NounsBuilder;
+
     /// @notice The WETH token address
-    address public immutable weth;
+    address private immutable WETH;
 
     ///                                                          ///
     ///                          CONSTRUCTOR                     ///
     ///                                                          ///
 
     /// @param _upgradeManager The address of the contract upgrade manager
+    /// @param _nouns The address of the Nouns Treasury
+    /// @param _nounsBuilder The address of the Nouns Builder
     /// @param _weth The WETH token address
-    constructor(address _upgradeManager, address _weth) payable initializer {
+    constructor(
+        address _upgradeManager,
+        address _nouns,
+        address _nounsBuilder,
+        address _weth
+    ) payable initializer {
         UpgradeManager = IUpgradeManager(_upgradeManager);
-        weth = _weth;
+        Nouns = _nouns;
+        NounsBuilder = _nounsBuilder;
+        WETH = _weth;
     }
 
     ///                                                          ///
     ///                          INITIALIZER                     ///
     ///                                                          ///
 
+    ///
     function initialize(
         address _token,
-        address _treasury,
+        address _foundersDAO,
         uint256 _timeBuffer,
         uint256 _reservePrice,
         uint256 _minBidIncrementPercentage,
         uint256 _duration
     ) external initializer {
-        // Initialize the proxy
-        __UUPSUpgradeable_init();
-
         // Initialize contract ownership
         __Ownable_init();
 
         // Transfer ownership to the treasury
-        transferOwnership(_treasury);
+        transferOwnership(_foundersDAO);
 
         // Initialize the reentrancy guard
         __ReentrancyGuard_init();
-
-        // Initialize the ability to pause the contract
-        __Pausable_init();
 
         // Pause the contract
         _pause();
@@ -72,7 +82,7 @@ contract AuctionHouse is AuctionHouseStorageV1, UUPSUpgradeable, PausableUpgrade
         // Store the address of the token to mint
         token = IToken(_token);
 
-        // Initialize the auction house metadata
+        // Store the auction house config
         timeBuffer = _timeBuffer;
         reservePrice = _reservePrice;
         minBidIncrementPercentage = _minBidIncrementPercentage;
@@ -84,13 +94,18 @@ contract AuctionHouse is AuctionHouseStorageV1, UUPSUpgradeable, PausableUpgrade
     ///                                                          ///
 
     /// @notice Emitted when a bid is placed
+    ///
+    ///
+    ///
+    ///
+    ///
     event AuctionBid(uint256 tokenId, address sender, uint256 value, bool extended, uint256 endTime);
 
     /// @notice Creates a bid for the current token
     /// @param _tokenId The ERC-721 token id
     function createBid(uint256 _tokenId) external payable nonReentrant {
-        // Cache the current auction in memory
-        Auction memory _auction = auction;
+        // Get the current auction in memory
+        IAuction.Auction memory _auction = auction;
 
         // Ensure the bid is for the current token id
         require(_auction.tokenId == _tokenId, "INVALID_TOKEN_ID");
@@ -131,48 +146,12 @@ contract AuctionHouse is AuctionHouseStorageV1, UUPSUpgradeable, PausableUpgrade
             auction.endTime = _auction.endTime = uint40(block.timestamp + timeBuffer);
         }
 
-        emit AuctionBid(_auction.tokenId, msg.sender, msg.value, extended, _auction.endTime);
+        emit AuctionBid(_tokenId, msg.sender, msg.value, extended, _auction.endTime);
     }
 
     ///                                                          ///
-    ///                    SETTLE AND CREATE AUCTION             ///
+    ///                          CREATE AUCTION                  ///
     ///                                                          ///
-
-    /// @notice Settles the current auction and creates the next one
-    function settleCurrentAndCreateNewAuction() external nonReentrant whenNotPaused {
-        _settleAuction();
-        _createAuction();
-    }
-
-    /// @notice Emitted when an auction is settled
-    event AuctionSettled(uint256 indexed nounId, address winner, uint256 amount);
-
-    /// @notice Settles the current auction
-    function _settleAuction() internal {
-        Auction memory _auction = auction;
-
-        require(_auction.startTime != 0, "NOT_STARTED");
-        require(!_auction.settled, "ALREADY_SETTLED");
-        require(block.timestamp >= _auction.endTime, "NOT_COMPLETED");
-
-        auction.settled = true;
-
-        // If there were no bids:
-        if (_auction.highestBidder == address(0)) {
-            // Burn the token
-            token.burn(_auction.tokenId);
-
-            // Else: transfer the the token to the winning bidder
-        } else {
-            token.transferFrom(address(this), _auction.highestBidder, _auction.tokenId);
-        }
-
-        if (_auction.highestBid > 0) {
-            _handleOutgoingTransfer(owner(), _auction.highestBid);
-        }
-
-        emit AuctionSettled(_auction.tokenId, _auction.highestBidder, _auction.highestBid);
-    }
 
     /// @notice Emitted when an auction is created
     event AuctionCreated(uint256 indexed nounId, uint256 startTime, uint256 endTime);
@@ -199,54 +178,72 @@ contract AuctionHouse is AuctionHouseStorageV1, UUPSUpgradeable, PausableUpgrade
     }
 
     ///                                                          ///
-    ///                       UPDATE TIME BUFFER                 ///
+    ///                         SETTLE AUCTION                   ///
     ///                                                          ///
 
-    /// @notice Emitted when the time buffer is updated
-    event AuctionTimeBufferUpdated(uint256 timeBuffer);
+    /// @notice Emitted when an auction is settled
+    event AuctionSettled(uint256 indexed nounId, address winner, uint256 amount);
 
-    /// @notice Updates the auction time buffer
-    /// @param _timeBuffer The time buffer to set
-    function setTimeBuffer(uint256 _timeBuffer) external onlyOwner {
-        // Update the time buffer
-        timeBuffer = _timeBuffer;
+    /// @notice Settles the current auction
+    function _settleAuction() internal {
+        IAuction.Auction memory _auction = auction;
 
-        emit AuctionTimeBufferUpdated(_timeBuffer);
+        require(_auction.startTime != 0, "NOT_STARTED");
+
+        require(!_auction.settled, "ALREADY_SETTLED");
+
+        require(block.timestamp >= _auction.endTime, "NOT_COMPLETED");
+
+        // Mark the auction as settled
+        auction.settled = true;
+
+        // If there were no bids:
+        if (_auction.highestBidder == address(0)) {
+            // Burn the token
+            token.burn(_auction.tokenId);
+
+            // Else: transfer the the token to the winning bidder
+        } else {
+            token.transferFrom(address(this), _auction.highestBidder, _auction.tokenId);
+        }
+
+        if (_auction.highestBid > 0) {
+            // Calculate 100 BPS of the highest bid to send to Nouns DAO and Nouns Builder DAO
+            uint256 fee = (_auction.highestBid * 100) / 10_000;
+
+            // Calculate the remaining profit
+            uint256 remainingProfit = _auction.highestBid - (2 * fee);
+
+            // Transfer to Nouns DAO
+            _handleOutgoingTransfer(Nouns, fee);
+
+            // Transfer to Nouns Builder DAO
+            _handleOutgoingTransfer(NounsBuilder, fee);
+
+            // Transfer the remaining profit to the treasury
+            _handleOutgoingTransfer(owner(), remainingProfit);
+        }
+
+        emit AuctionSettled(_auction.tokenId, _auction.highestBidder, _auction.highestBid);
+    }
+
+    /// @notice Settle the auction when the contract is paused
+    function settleAuction() external whenPaused nonReentrant {
+        _settleAuction();
     }
 
     ///                                                          ///
-    ///                      UPDATE RESERVE PRICE                ///
+    ///                    SETTLE & CREATE AUCTION               ///
     ///                                                          ///
 
-    /// @notice Emitted when the reserve price is updated
-    event AuctionReservePriceUpdated(uint256 reservePrice);
-
-    /// @notice Updates the auction reserve price
-    /// @param _reservePrice The reserve price to set
-    function setReservePrice(uint256 _reservePrice) external onlyOwner {
-        // Update the reserve price
-        reservePrice = _reservePrice;
-
-        emit AuctionReservePriceUpdated(_reservePrice);
+    /// @notice Settles the current auction and creates the next one
+    function settleCurrentAndCreateNewAuction() external nonReentrant whenNotPaused {
+        _settleAuction();
+        _createAuction();
     }
 
     ///                                                          ///
-    ///                 UPDATE MINIMUM BID INCREMENT             ///
-    ///                                                          ///
-
-    /// @notice Emitted when the min bid increment percentage is updated
-    event AuctionMinBidIncrementPercentageUpdated(uint256 minBidIncrementPercentage);
-
-    /// @notice Updates the auction minimum bid increment percentage
-    /// @param _minBidIncrementPercentage The min bid increment percentage to set
-    function setMinBidIncrementPercentage(uint256 _minBidIncrementPercentage) external onlyOwner {
-        minBidIncrementPercentage = _minBidIncrementPercentage;
-
-        emit AuctionMinBidIncrementPercentageUpdated(_minBidIncrementPercentage);
-    }
-
-    ///                                                          ///
-    ///                           DAO PAUSE                      ///
+    ///                             PAUSE                        ///
     ///                                                          ///
 
     /// @notice Pause the auction house
@@ -254,19 +251,20 @@ contract AuctionHouse is AuctionHouseStorageV1, UUPSUpgradeable, PausableUpgrade
         _pause();
     }
 
-    /// @notice Unpause the auction house
+    /// @notice Unpauses the auction house and starts the next auction
     function unpause() external onlyOwner {
         _unpause();
 
+        // If either this is the first auction or the previous auction was settled:
         if (auction.startTime == 0 || auction.settled) {
+            // Create a new auction
             _createAuction();
         }
     }
 
-    /// @notice Settle the auction when the contract is paused
-    function settleAuction() external whenPaused nonReentrant {
-        _settleAuction();
-    }
+    ///                                                          ///
+    ///                                                          ///
+    ///                                                          ///
 
     ///                                                          ///
     ///                          ETH TRANSFER                    ///
@@ -282,8 +280,8 @@ contract AuctionHouse is AuctionHouseStorageV1, UUPSUpgradeable, PausableUpgrade
 
         // If fails, wrap and send as WETH
         if (!success) {
-            IWETH(weth).deposit{value: _amount}();
-            IERC20(weth).transfer(_dest, _amount);
+            IWETH(WETH).deposit{value: _amount}();
+            IERC20(WETH).transfer(_dest, _amount);
         }
     }
 
