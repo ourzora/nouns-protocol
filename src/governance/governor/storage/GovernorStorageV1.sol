9abcc9303f9d51524303b6e468367c68d09d984c
Rohan Kulkarni <rohan@zora.co>
2022-07-09 19:41:59 -0700

    refactor: pack governor storage

--- src/governance/governor/storage/GovernorStorageV1.sol
+++ src/governance/governor/storage/GovernorStorageV1.sol
@@ -1,28 +1,12 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.10;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.15;
 
-import {ITreasury} from "../../treasury/ITreasury.sol";
-import {IToken} from "../../../token/IToken.sol";
+import {IGovernor} from "../IGovernor.sol";
 
 /// @title Governor Storage V1
 /// @author Rohan Kulkarni
 /// @notice Modified version of NounsDAOInterfaces.sol (commit 2cbe6c7) that NounsDAO licensed under the GPL-3.0 license
 contract GovernorStorageV1 {
-    /// @notice The address of the DAO treasury
-    ITreasury public treasury;
-
-    /// @notice The address of the token
-    IToken public token;
-
-    /// @notice The number of blocks that voting is delayed after a proposal
-    uint256 internal VOTING_DELAY;
-
-    /// @notice The number of blocks that voting takes place after a proposal
-    uint256 internal VOTING_PERIOD;
-
-    /// @notice The number of votes required in order for a voter to become a proposer
-    uint256 internal PROPOSAL_THRESHOLD_BPS;
-
-    /// @notice The number of votes required to support a proposal in order for a quorum to be reached and for a vote to succeed
-    uint256 internal QUORUM_VOTES_BPS;
+    /// @notice The metadata of the governor
+    IGovernor.GovMeta public govMeta;
 }
