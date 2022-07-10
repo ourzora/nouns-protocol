419d0ec9c7047b7cc405c37e93c2e3a2fd678920
Rohan Kulkarni <rohan@zora.co>
2022-07-09 19:13:36 -0700

    refactor: token init, vesting, and auto delegation

--- src/token/Token.sol
+++ src/token/Token.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.10;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.15;
 
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
@@ -40,20 +40,19 @@ contract Token is UUPSUpgradeable, ReentrancyGuardUpgradeable, OwnableUpgradeabl
     ///                          INITIALIZER                     ///
     ///                                                          ///
 
-    /// @notice Called by the deployer to initialize the token proxy
-    /// @param _name The token name
-    /// @param _symbol The token $SYMBOL
+    /// @notice Initializes an ERC-1967 proxy instance of the token
+    /// @param _initData The encoded token and metadata init strings
     /// @param _foundersDAO The address of the founders DAO
-    /// @param _foundersMaxAllocation The maximum number of tokens the founders will vest (eg. 183 nouns to nounders)
+    /// @param _foundersMaxTokens The maximum number of tokens the founders will vest (eg. 183 nouns to nounders)
     /// @param _foundersAllocationFrequency The allocation frequency (eg. every 10 nouns)
     /// @param _auction The address of the auction house that will mint tokens
     function initialize(
-        string calldata _name,
-        string calldata _symbol,
+        bytes calldata _initData,
         address _foundersDAO,
-        uint256 _foundersMaxAllocation,
+        uint256 _foundersMaxTokens,
         uint256 _foundersAllocationFrequency,
-        address _auction
+        address _auction,
+        address _treasury
     ) public initializer {
         // Initialize the reentrancy guard
         __ReentrancyGuard_init();
@@ -61,22 +60,40 @@ contract Token is UUPSUpgradeable, ReentrancyGuardUpgradeable, OwnableUpgradeabl
         // Initialize contract ownership
         __Ownable_init();
 
-        // Transfer ownership to the founders DAO
-        transferOwnership(_foundersDAO);
+        // Transfer ownership to the DAO treasury
+        transferOwnership(_treasury);
+
+        // Decode the tuple of init strings
+        (string memory _name, string memory _symbol, string memory _description, string memory _contractImage, string memory _rendererBase) = abi
+            .decode(_initData, (string, string, string, string, string));
 
         // Initialize the ERC-721 token
         __ERC721_init(_name, _symbol);
 
         // Store the founders metadata
         founders.DAO = _foundersDAO;
-        founders.maxAllocation = uint32(_foundersMaxAllocation);
+        founders.maxAllocation = uint32(_foundersMaxTokens);
         founders.allocationFrequency = uint32(_foundersAllocationFrequency);
 
         // Store the address allowed to mint tokens
         auction = _auction;
 
-        // Deploy and store the metadata renderer
-        metadataRenderer = IMetadataRenderer(address(new ERC1967Proxy(metadataImpl, abi.encodeWithSignature("initialize(address)", _foundersDAO))));
+        // Deploy the associated metadata renderer
+        metadataRenderer = IMetadataRenderer(
+            address(
+                new ERC1967Proxy(
+                    metadataImpl,
+                    abi.encodeWithSignature(
+                        "initialize(address,string,string,string,string)",
+                        _foundersDAO,
+                        _name,
+                        _description,
+                        _contractImage,
+                        _rendererBase
+                    )
+                )
+            )
+        );
     }
 
     ///                                                          ///
@@ -88,21 +105,50 @@ contract Token is UUPSUpgradeable, ReentrancyGuardUpgradeable, OwnableUpgradeabl
         // Ensure the caller is the auction house
         require(msg.sender == auction, "ONLY_AUCTION");
 
+        // Used to store the next token id
+        uint256 tokenId;
+
+        //
         unchecked {
-            // If the token belongs to the founders:
-            if (founders.currentAllocation < founders.maxAllocation && totalSupply % founders.allocationFrequency == 0) {
-                // Send the token to the founders
-                _mint(founders.DAO, totalSupply++);
-
-                // Update their vested allocation
-                ++founders.currentAllocation;
-            }
-
-            // Mint the next token for bidding
-            _mint(auction, totalSupply++);
-
-            return totalSupply - 1;
+            //
+            tokenId = totalSupply++;
         }
+
+        // If the token belongs to the founders:
+        if (_isFoundersVest(tokenId)) {
+            // Send the token to the founders
+            _mint(founders.DAO, tokenId);
+
+            unchecked {
+                // Update the number of vested tokens
+                ++founders.currentAllocation;
+
+                // Get the next token id
+                tokenId = totalSupply++;
+            }
+        }
+
+        // Send the next token to the auction house for bidding
+        _mint(auction, tokenId);
+
+        return tokenId;
+    }
+
+    function _isFoundersVest(uint256 _tokenId) private view returns (bool valid) {
+        uint256 currentAllocation = founders.currentAllocation;
+        uint256 maxAllocation = founders.maxAllocation;
+        uint256 allocationFrequency = founders.allocationFrequency;
+
+        assembly {
+            // if (founders.currentAllocation < founders.maxAllocation && _tokenId % founders.allocationFrequency == 0);
+            valid := and(lt(currentAllocation, maxAllocation), iszero(mod(_tokenId, allocationFrequency)))
+        }
+    }
+
+    function _mint(address _to, uint256 _tokenId) internal override {
+        super._mint(_to, _tokenId);
+
+        metadataRenderer.generate(_tokenId);
     }
 
     ///                                                          ///
@@ -135,23 +181,6 @@ contract Token is UUPSUpgradeable, ReentrancyGuardUpgradeable, OwnableUpgradeabl
     }
 
     ///                                                          ///
-    ///                         UPDATE AUCTION                   ///
-    ///                                                          ///
-
-    /// @notice Emitted when the auction is updated
-    /// @param auction The address of the new auction
-    event AuctionUpdated(address auction);
-
-    /// @notice Updates the address of the auction
-    /// @param _auction The address of the auction to set
-    function setAuction(address _auction) external onlyOwner {
-        // Update the auction address
-        auction = _auction;
-
-        emit AuctionUpdated(_auction);
-    }
-
-    ///                                                          ///
     ///                        UPDATE FOUNDERS                   ///
     ///                                                          ///
 
@@ -162,13 +191,41 @@ contract Token is UUPSUpgradeable, ReentrancyGuardUpgradeable, OwnableUpgradeabl
     /// @notice Updates the address of the founders DAO
     /// @param _foundersDAO The address of the founders DAO to set
     function setFoundersDAO(address _foundersDAO) external {
-        // Ensure the caller is the founders
+        // Ensure the caller is the founders DAO
         require(msg.sender == founders.DAO, "ONLY_FOUNDERS");
 
         // Update the founders DAO address
         founders.DAO = _foundersDAO;
 
         emit FoundersDAOUpdated(_foundersDAO);
+    }
+
+    event FoundersVestingAllocation(uint256 numTokens);
+
+    function setFoundersVestingAllocation(uint256 _numTokens) external onlyOwner {
+        require(_numTokens < type(uint32).max, "");
+
+        founders.maxAllocation = uint32(_numTokens);
+
+        emit FoundersVestingAllocation(_numTokens);
+    }
+
+    event FoundersVestingSchedule(uint256 numTokens);
+
+    function setFoundersVestingSchedule(uint256 _tokenInterval) external onlyOwner {
+        require(_tokenInterval < type(uint32).max, "");
+
+        founders.allocationFrequency = uint32(_tokenInterval);
+
+        emit FoundersVestingSchedule(_tokenInterval);
+    }
+
+    function autoDelegate(address _user) external {
+        // Ensure the caller is the auction house
+        require(msg.sender == auction, "ONLY_AUCTION");
+
+        //
+        _delegate(_user, _user);
     }
 
     ///                                                          ///
