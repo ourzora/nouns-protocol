ca6e0f549c8534ac996717f401e914128395c0bd
Rohan Kulkarni <rohan@zora.co>
2022-07-09 18:54:01 -0700

    refactor: update renderer initialization & various optimizations

--- src/token/metadata/MetadataRenderer.sol
+++ src/token/metadata/MetadataRenderer.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.10;
+pragma solidity 0.8.15;
 
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
@@ -7,31 +7,210 @@ import {StringsUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/Stri
 import {LibUintToString} from "sol2string/LibUintToString.sol";
 
 import {MetadataRendererStorageV1} from "./storage/MetadataRendererStorageV1.sol";
-import {Entropy} from "./Entropy.sol";
-
 import {IToken} from "../IToken.sol";
 import {IMetadataRenderer} from "./IMetadataRenderer.sol";
 import {IUpgradeManager} from "../../upgrade/IUpgradeManager.sol";
 
-contract MetadataRenderer is UUPSUpgradeable, OwnableUpgradeable, Entropy, MetadataRendererStorageV1, IMetadataRenderer {
+/// @title Metadata Renderer
+/// @author Iain Nash & Rohan Kulkarni
+/// @notice
+contract MetadataRenderer is IMetadataRenderer, UUPSUpgradeable, OwnableUpgradeable, MetadataRendererStorageV1 {
     ///                                                          ///
     ///                                                          ///
     ///                                                          ///
 
-    constructor() payable initializer {
-        // prevent calling directly
+    constructor() payable initializer {}
+
+    ///                                                          ///
+    ///                                                          ///
+    ///                                                          ///
+
+    function initialize(
+        address _foundersDAO,
+        string calldata _name,
+        string calldata _description,
+        string calldata _contractImage,
+        string calldata _rendererBase
+    ) external initializer {
+        // Initialize ownership
+        __Ownable_init();
+
+        // Store the associated token
+        token = IToken(msg.sender);
+
+        //
+        name = _name;
+        description = _description;
+        contractImage = _contractImage;
+        rendererBase = _rendererBase;
+
+        // Transfer ownership to the founders
+        transferOwnership(_foundersDAO);
     }
 
     ///                                                          ///
     ///                                                          ///
     ///                                                          ///
 
-    function initialize(address _foundersDAO) external initializer {
-        token = IToken(msg.sender);
+    function propertiesCount() external view returns (uint256) {
+        return properties.length;
+    }
 
-        __Ownable_init();
+    function itemsCount(uint256 _propertyId) external view returns (uint256) {
+        return properties[_propertyId].items.length;
+    }
 
-        transferOwnership(_foundersDAO);
+    ///                                                          ///
+    ///                                                          ///
+    ///                                                          ///
+
+    event PropertyAdded(uint256 id, string name);
+
+    event ItemAdded(uint256 propertyId, uint256 itemIndex);
+
+    function addProperties(
+        string[] calldata _names,
+        ItemParam[] calldata _items,
+        bytes calldata _data
+    ) external onlyOwner {
+        //
+        uint256 dataLength = data.length;
+
+        //
+        if (_data.length > 0) {
+            //
+            data.push(_data);
+        }
+
+        // Cache the number of properties that already exist
+        uint256 numStoredProperties = properties.length;
+
+        // Cache the number of new properties to add
+        uint256 numNewProperties = _names.length;
+
+        // Cache the number of new items to add
+        uint256 numNewItems = _items.length;
+
+        // Used to store each new property id
+        uint256 propertyId;
+
+        // For each new property:
+        for (uint256 i = 0; i < numNewProperties; ) {
+            // Append one slot of storage space
+            properties.push();
+
+            unchecked {
+                // Compute the property id
+                propertyId = numStoredProperties + i;
+            }
+
+            // Store the property name
+            properties[propertyId].name = _names[i];
+
+            emit PropertyAdded(propertyId, name);
+
+            unchecked {
+                ++i;
+            }
+        }
+
+        // For each new item:
+        for (uint256 i = 0; i < numNewItems; ) {
+            // Cache its property id
+            uint256 _propertyId = _items[i].propertyId;
+
+            // 100 - x = uses only new properties x >= 100
+            // x = uses current properties where x < 100
+
+            //
+            if (_propertyId >= 100) {
+                //
+                unchecked {
+                    _propertyId += numStoredProperties - 100;
+                }
+            }
+
+            // Get the storage location of its property's items
+            Item[] storage propertyItems = properties[_propertyId].items;
+
+            // Append one slot of storage space
+            propertyItems.push();
+
+            // Used to store the new item
+            Item storage newItem;
+
+            // Used to store the index of the new item
+            uint256 newItemIndex;
+
+            // Cannot underflow as `propertyItems.length` is ensured to be at least 1
+            unchecked {
+                //
+                newItemIndex = propertyItems.length - 1;
+            }
+
+            // Store the new item
+            newItem = propertyItems[newItemIndex];
+
+            // Store its associated metadata
+            newItem.dataType = uint8(_items[i].dataType);
+            newItem.info = _items[i].info;
+            newItem.name = _items[i].name;
+            newItem.referenceSlot = uint16(dataLength);
+
+            emit ItemAdded(_propertyId, newItemIndex);
+
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    ///                                                          ///
+    ///                                                          ///
+    ///                                                          ///
+
+    function generate(uint256 _tokenId) external {
+        // Ensure the caller is the token contract
+        require(msg.sender == address(token), "ONLY_TOKEN");
+
+        // Compute some randomness
+        uint256 entropy = _getEntropy(_tokenId);
+
+        // Get the storage location for the token attributes
+        uint16[11] storage tokenAttributes = attributes[_tokenId];
+
+        // Cache the number of properties to choose from
+        uint256 numProperties = properties.length;
+
+        // Store that number for future reference
+        tokenAttributes[0] = uint16(numProperties);
+
+        // Used to store the number of items choosing from each property
+        uint256 numItems;
+
+        // For each property:
+        for (uint256 i = 0; i < numProperties; ) {
+            // Get the number of items
+            numItems = properties[i].items.length;
+
+            unchecked {
+                // Use the previously generated randomness to choose one item in the property
+                tokenAttributes[i + 1] = uint16(entropy % numItems);
+
+                // Adjust the randomness
+                entropy >>= 16;
+
+                ++i;
+            }
+        }
+    }
+
+    ///                                                          ///
+    ///                                                          ///
+    ///                                                          ///
+
+    function _getEntropy(uint256 seed) private view returns (uint256) {
+        return uint256(keccak256(abi.encode(seed, blockhash(block.number), block.coinbase, block.timestamp)));
     }
 
     ///                                                          ///
@@ -42,15 +221,15 @@ contract MetadataRenderer is UUPSUpgradeable, OwnableUpgradeable, Entropy, Metad
         return string(abi.encodePacked('{"name": "', name, '", "description": "', description, '", "image": "', contractImage, '"}'));
     }
 
-    function tokenURI(uint256 tokenId) external view returns (string memory) {
-        (bytes memory propertiesAry, bytes memory propertiesQuery) = getProperties(tokenId);
+    function tokenURI(uint256 _tokenId) external view returns (string memory) {
+        (bytes memory propertiesAry, bytes memory propertiesQuery) = getProperties(_tokenId);
         return
             string(
                 abi.encodePacked(
                     '{"name": "',
                     name,
                     " #",
-                    LibUintToString.toString(tokenId),
+                    LibUintToString.toString(_tokenId),
                     '", "description": "',
                     description,
                     '", "image": "',
@@ -67,131 +246,79 @@ contract MetadataRenderer is UUPSUpgradeable, OwnableUpgradeable, Entropy, Metad
     ///                                                          ///
     ///                                                          ///
 
-    function setAllMetadata(bytes memory data) external onlyOwner {
-        (string memory _name, string memory _description, string memory _contractImage, string memory _rendererBase) = abi.decode(
-            data,
-            (string, string, string, string)
-        );
+    function getProperties(uint256 _tokenId) public view returns (bytes memory aryAttributes, bytes memory queryString) {
+        // Get the attributes for the given token
+        uint16[11] memory tokenAttributes = attributes[_tokenId];
 
-        name = _name;
-        description = _description;
-        contractImage = _contractImage;
-        rendererBase = _rendererBase;
-    }
-
-    ///                                                          ///
-    ///                                                          ///
-    ///                                                          ///
-
-    event NewContractImage(string indexed);
-
-    function setContractImage(string memory newContractImage) public onlyOwner {
-        contractImage = newContractImage;
-
-        emit NewContractImage(contractImage);
-    }
-
-    ///                                                          ///
-    ///                                                          ///
-    ///                                                          ///
-
-    event NewBaseRenderer(string indexed);
-
-    function setBaseRenderer(string memory newRenderer) public onlyOwner {
-        rendererBase = newRenderer;
-
-        emit NewBaseRenderer(newRenderer);
-    }
-
-    ///                                                          ///
-    ///                                                          ///
-    ///                                                          ///
-
-    function addProperties(
-        string[] memory _newProperties,
-        ItemInfoStorage[] memory _items,
-        bytes memory _data
-    ) external onlyOwner {
-        uint256 propertiesBaseLength = properties.length;
-        uint256 dataBaseLength = data.length;
-
-        if (_data.length > 0) {
-            data.push(_data);
-        }
-
-        for (uint256 i = 0; i < _newProperties.length; i++) {
-            properties.push();
-            properties[propertiesBaseLength + i].name = _newProperties[i];
-        }
-
-        for (uint256 i = 0; i < _items.length; i++) {
-            // 100 - x = uses only new properties x >= 100
-            // x = uses current properties where x < 100
-            uint256 id = _items[i].propertyId >= 100 ? _items[i].propertyId + propertiesBaseLength - 100 : _items[i].propertyId;
-            properties[id].items.push();
-            uint256 newLength = properties[id].items.length;
-            Item storage item = properties[id].items[newLength - 1];
-            item.dataType = _items[i].dataType;
-            item.referenceSlot = uint16(dataBaseLength);
-            item.info = _items[i].info;
-            item.name = _items[i].name;
-        }
-    }
-
-    ///                                                          ///
-    ///                                                          ///
-    ///                                                          ///
-
-    function getProperties(uint256 tokenId) public view returns (bytes memory aryAttributes, bytes memory queryString) {
-        uint16[11] storage atAttributes = chosenAttributes[tokenId];
+        // Compute its query string
         queryString = abi.encodePacked(
             "contractAddress=",
             StringsUpgradeable.toHexString(uint256(uint160(address(this))), 20),
             "&tokenId=",
-            StringsUpgradeable.toString(tokenId)
+            StringsUpgradeable.toString(_tokenId)
         );
 
-        for (uint256 i = 0; i < atAttributes[0]; i++) {
-            bool isLast = i == atAttributes[0] - 1;
-            Item storage item = properties[i].items[atAttributes[i + 1]];
-            string memory propertyName = properties[i].name;
-            aryAttributes = abi.encodePacked(aryAttributes, '"', propertyName, '": "', item.name, '"', isLast ? "" : ",");
-            queryString = abi.encodePacked(queryString, "&", propertyName, "=", item.name, "&images[]=", _getImageForItem(item, propertyName), "&");
+        // Cache its number of properties
+        uint256 numProperties = tokenAttributes[0];
+
+        // Used to hold the property and item data during
+        Property memory property;
+        Item memory item;
+
+        // Used to cache the property and item names
+        string memory propertyName;
+        string memory itemName;
+
+        // Used to get the item data of each generated attribute
+        uint256 attribute;
+
+        // Used to
+        bool isLast;
+
+        // For each of the token's properties:
+        for (uint256 i = 0; i < numProperties; ) {
+            //
+            unchecked {
+                // Check if this is the last iteration
+                isLast = i == (numProperties - 1);
+            }
+
+            // Get the property data
+            property = properties[i];
+
+            unchecked {
+                // Get the index of its generated attribute for this property
+                attribute = tokenAttributes[i + 1];
+            }
+
+            // Get the associated item data
+            item = property.items[attribute];
+
+            // Cache the names of the property and item
+            propertyName = property.name;
+            itemName = item.name;
+
+            //
+            aryAttributes = abi.encodePacked(aryAttributes, '"', propertyName, '": "', itemName, '"', isLast ? "" : ",");
+            queryString = abi.encodePacked(queryString, "&", propertyName, "=", itemName, "&images[]=", _getImageForItem(item, propertyName), "&");
+
+            unchecked {
+                ++i;
+            }
         }
     }
 
-    function _getImageForItem(Item storage item, string memory propertyName) internal view returns (string memory) {
-        if (item.dataType == DATA_TYPE_IPFS_SINGULAR) {
-            return string(abi.encodePacked("ipfs://", item.info));
-        }
-        if (item.dataType == DATA_TYPE_IPFS_GROUP) {
-            (string memory base, string memory postfix) = abi.decode(data[item.referenceSlot], (string, string));
-            return string(abi.encodePacked("ipfs://", base, "/", propertyName, "/", item.name, postfix));
-        }
-        if (item.dataType == DATA_TYPE_CENTRALIZED) {
+    function _getImageForItem(Item memory _item, string memory _propertyName) internal view returns (string memory) {
+        if (_item.dataType == DATA_TYPE_IPFS_SINGULAR) {
+            return string(abi.encodePacked("ipfs://", _item.info));
+        } else if (_item.dataType == DATA_TYPE_IPFS_GROUP) {
+            (string memory base, string memory postfix) = abi.decode(data[_item.referenceSlot], (string, string));
+            return string(abi.encodePacked("ipfs://", base, "/", _propertyName, "/", _item.name, postfix));
+        } else if (_item.dataType == DATA_TYPE_CENTRALIZED) {
             // ignore image paths for centralized, pass in specific data if needed.
-            return string(abi.encodePacked("data:", item.info));
-        }
-        return "";
-    }
-
-    ///                                                          ///
-    ///                                                          ///
-    ///                                                          ///
-
-    function minted(uint256 tokenId) external {
-        require(msg.sender == address(token), "ONLY_TOKEN");
-
-        uint256 entropy = _getEntropy(tokenId);
-
-        uint16[11] storage atAttributes = chosenAttributes[tokenId];
-
-        atAttributes[0] = uint16(properties.length);
-
-        for (uint256 i = 0; i < properties.length; i++) {
-            uint16 size = uint16(properties[i].items.length);
-            atAttributes[i + 1] = uint16(entropy) % size;
-            entropy >>= 16;
+            return string(abi.encodePacked("data:", _item.info));
+        } else {
+            return "";
         }
     }
 
