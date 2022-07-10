6d22a278e8374f5f472d74815cb743559ca0fddc
Rohan Kulkarni <rohan@zora.co>
2022-07-10 16:35:25 -0700

    chore: remove outdated renderer files

--- src/token/metadata/Entropy.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.10;
-
-contract Entropy {
-    function _getEntropy(uint256 seed) internal view returns (uint256) {
-        return uint256(keccak256(abi.encode(seed, blockhash(block.number), block.coinbase, block.timestamp)));
-    }
-}
