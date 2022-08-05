e83ea1865a13a51149135f8acc2b2e6a42ba546c
Rohan Kulkarni <rohan@zora.co>
2022-08-05 14:09:40 -0400

    refactor: add ERC721Votes

---
+++ src/lib/token/ERC721.sol
@@ -0,0 +1,186 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.15;
+
+import {Initializable} from "../proxy/Initializable.sol";
+import {Address} from "../utils/Address.sol";
+import {Strings} from "../utils/Strings.sol";
+import {ERC721TokenReceiver} from "../utils/TokenReceiver.sol";
+
+contract ERC721StorageV1 {
+    string public name;
+
+    string public symbol;
+
+    uint256 public totalSupply;
+
+    mapping(uint256 => address) public getApproved;
+
+    mapping(address => mapping(address => bool)) public isApprovedForAll;
+
+    mapping(uint256 => address) internal _ownerOf;
+
+    mapping(address => uint256) internal _balanceOf;
+}
+
+abstract contract ERC721 is Initializable, ERC721StorageV1 {
+    event Approval(address indexed owner, address indexed spender, uint256 indexed tokenId);
+
+    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+
+    function __ERC721_init(string memory _name, string memory _symbol) internal onlyInitializing {
+        name = _name;
+        symbol = _symbol;
+    }
+
+    function tokenURI(uint256 _tokenId) public view virtual returns (string memory) {}
+
+    function contractURI() public view virtual returns (string memory) {}
+
+    function supportsInterface(bytes4 interfaceId) public view virtual returns (bool) {
+        return
+            interfaceId == 0x01ffc9a7 || // ERC165 Interface ID
+            interfaceId == 0x80ac58cd || // ERC721 Interface ID
+            interfaceId == 0x5b5e139f; // ERC721Metadata Interface ID
+    }
+
+    function balanceOf(address _owner) public view virtual returns (uint256) {
+        require(_owner != address(0), "ZERO_ADDRESS");
+
+        return _balanceOf[_owner];
+    }
+
+    function ownerOf(uint256 _tokenId) public view virtual returns (address) {
+        address owner = _ownerOf[_tokenId];
+
+        require(owner != address(0), "ZERO_ADDRESS");
+
+        return owner;
+    }
+
+    function approve(address _to, uint256 _tokenId) public virtual {
+        address owner = _ownerOf[_tokenId];
+
+        require(msg.sender == owner || isApprovedForAll[owner][msg.sender], "NOT_AUTHORIZED");
+
+        getApproved[_tokenId] = _to;
+
+        emit Approval(owner, _to, _tokenId);
+    }
+
+    function setApprovalForAll(address _operator, bool _approved) public virtual {
+        isApprovedForAll[msg.sender][_operator] = _approved;
+
+        emit ApprovalForAll(msg.sender, _operator, _approved);
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) public virtual {
+        require(_from == _ownerOf[_tokenId], "WRONG_FROM");
+
+        require(_to != address(0), "INVALID_RECIPIENT");
+
+        require(msg.sender == _from || isApprovedForAll[_from][msg.sender] || msg.sender == getApproved[_tokenId], "NOT_AUTHORIZED");
+
+        _beforeTokenTransfer(_from, _to, _tokenId);
+
+        unchecked {
+            --_balanceOf[_from];
+
+            ++_balanceOf[_to];
+        }
+
+        _ownerOf[_tokenId] = _to;
+
+        delete getApproved[_tokenId];
+
+        emit Transfer(_from, _to, _tokenId);
+
+        _afterTokenTransfer(_from, _to, _tokenId);
+    }
+
+    function safeTransferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) public virtual {
+        transferFrom(_from, _to, _tokenId);
+
+        require(
+            !Address.isContract(_to) ||
+                ERC721TokenReceiver(_to).onERC721Received(msg.sender, _from, _tokenId, "") == ERC721TokenReceiver.onERC721Received.selector,
+            "UNSAFE_RECIPIENT"
+        );
+    }
+
+    function safeTransferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId,
+        bytes calldata _data
+    ) public virtual {
+        transferFrom(_from, _to, _tokenId);
+
+        require(
+            !Address.isContract(_to) ||
+                ERC721TokenReceiver(_to).onERC721Received(msg.sender, _from, _tokenId, _data) == ERC721TokenReceiver.onERC721Received.selector,
+            "UNSAFE_RECIPIENT"
+        );
+    }
+
+    function _mint(address _to, uint256 _tokenId) internal virtual {
+        require(_to != address(0), "INVALID_RECIPIENT");
+
+        require(_ownerOf[_tokenId] == address(0), "ALREADY_MINTED");
+
+        _beforeTokenTransfer(address(0), _to, _tokenId);
+
+        unchecked {
+            ++totalSupply;
+            ++_balanceOf[_to];
+        }
+
+        _ownerOf[_tokenId] = _to;
+
+        emit Transfer(address(0), _to, _tokenId);
+
+        _afterTokenTransfer(address(0), _to, _tokenId);
+    }
+
+    function _burn(uint256 _tokenId) internal virtual {
+        address owner = _ownerOf[_tokenId];
+
+        require(owner != address(0), "NOT_MINTED");
+
+        _beforeTokenTransfer(owner, address(0), _tokenId);
+
+        unchecked {
+            --totalSupply;
+            --_balanceOf[owner];
+        }
+
+        delete _ownerOf[_tokenId];
+
+        delete getApproved[_tokenId];
+
+        emit Transfer(owner, address(0), _tokenId);
+
+        _afterTokenTransfer(owner, address(0), _tokenId);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal virtual {}
+
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal virtual {}
+}
---
+++ src/lib/token/ERC721Votes.sol
@@ -0,0 +1,182 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.15;
+
+import {ECDSA} from "../utils/ECDSA.sol";
+import {EIP712} from "../utils/EIP712.sol";
+import {ERC721} from "../token/ERC721.sol";
+
+contract ERC721VotesTypesV1 {
+    struct Checkpoint {
+        uint64 timestamp;
+        uint192 votes;
+    }
+}
+
+contract ERC721VotesStorageV1 is ERC721VotesTypesV1 {
+    mapping(address => mapping(uint256 => Checkpoint)) public checkpoints;
+
+    mapping(address => uint256) public numCheckpoints;
+
+    mapping(address => address) public delegates;
+
+    mapping(address => uint256) internal nonces;
+}
+
+abstract contract ERC721Votes is EIP712, ERC721, ERC721VotesStorageV1 {
+    bytes32 internal constant DELEGATION_TYPEHASH = keccak256("Delegation(address delegatee,uint256 nonce,uint256 expiry)");
+
+    /// @dev Emitted when an account changes their delegate.
+    event DelegateChanged(address indexed delegator, address indexed fromDelegate, address indexed toDelegate);
+
+    /// @dev Emitted when a token transfer or delegate change results in changes to a delegate's number of votes
+    event DelegateVotesChanged(address indexed delegate, uint256 previousBalance, uint256 newBalance);
+
+    /// @dev Delegates votes from the sender to `_delegatee`
+    function delegate(address _delegatee) public virtual {
+        _delegate(msg.sender, _delegatee);
+    }
+
+    ///
+    function delegateBySig(
+        address delegatee,
+        uint256 nonce,
+        uint256 expiry,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) public virtual {
+        require(block.timestamp <= expiry, "EXPIRED_SIG");
+
+        address signer = ECDSA.recover(_hashTypedDataV4(keccak256(abi.encode(DELEGATION_TYPEHASH, delegatee, nonce, expiry))), v, r, s);
+
+        unchecked {
+            require(nonce == nonces[signer]++, "INVALID_NONCE");
+        }
+
+        _delegate(signer, delegatee);
+    }
+
+    /// @dev Delegate all of `account`'s voting units to `delegatee`.
+    function _delegate(address _account, address _delegatee) internal virtual {
+        address prevDelegate = delegates[_account];
+
+        delegates[_account] = _delegatee;
+
+        emit DelegateChanged(_account, prevDelegate, _delegatee);
+
+        _moveDelegateVotes(prevDelegate, _delegatee, balanceOf(_account));
+    }
+
+    /// @dev Moves delegated votes from one delegate to another.
+    function _moveDelegateVotes(
+        address _from,
+        address _to,
+        uint256 _amount
+    ) internal {
+        unchecked {
+            if (_from != _to && _amount > 0) {
+                if (_from != address(0)) {
+                    uint256 nCheckpoints = numCheckpoints[_from]++;
+
+                    uint256 prevTotalVotes;
+
+                    if (nCheckpoints != 0) {
+                        prevTotalVotes = checkpoints[_from][nCheckpoints - 1].votes;
+                    }
+
+                    uint256 newTotalVotes = prevTotalVotes - _amount;
+
+                    Checkpoint storage checkpoint = checkpoints[_from][nCheckpoints];
+
+                    checkpoint.votes = uint192(newTotalVotes);
+                    checkpoint.timestamp = uint64(block.timestamp);
+
+                    emit DelegateVotesChanged(_from, prevTotalVotes, newTotalVotes);
+                }
+
+                if (_to != address(0)) {
+                    uint256 nCheckpoints = numCheckpoints[_to]++;
+
+                    uint256 prevTotalVotes;
+
+                    if (nCheckpoints != 0) {
+                        prevTotalVotes = checkpoints[_to][nCheckpoints - 1].votes;
+                    }
+
+                    uint256 newTotalVotes = prevTotalVotes + _amount;
+
+                    Checkpoint storage checkpoint = checkpoints[_to][nCheckpoints];
+
+                    checkpoint.votes = uint192(newTotalVotes);
+                    checkpoint.timestamp = uint64(block.timestamp);
+
+                    emit DelegateVotesChanged(_to, prevTotalVotes, newTotalVotes);
+                }
+            }
+        }
+    }
+
+    function _afterTokenTransfer(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) internal virtual override {
+        _moveDelegateVotes(_from, _to, 1);
+
+        super._afterTokenTransfer(_from, _to, _tokenId);
+    }
+
+    ///                                                          ///
+    ///                                                          ///
+    ///                                                          ///
+
+    /// @notice The current amount of votes that `_account` has.
+    function getVotes(address _account) public view returns (uint256) {
+        uint256 nCheckpoints = numCheckpoints[_account];
+
+        unchecked {
+            return nCheckpoints != 0 ? checkpoints[_account][nCheckpoints - 1].votes : 0;
+        }
+    }
+
+    /// @notice Returns the amount of votes that `_account` had at the end of a past timestamp.
+    function getPastVotes(address _account, uint256 _timestamp) public view virtual returns (uint256) {
+        require(_timestamp < block.timestamp, "INVALID_TIMESTAMP");
+
+        uint256 nCheckpoints = numCheckpoints[_account];
+
+        if (nCheckpoints == 0) return 0;
+
+        mapping(uint256 => Checkpoint) storage accountCheckpoints = checkpoints[_account];
+
+        if (accountCheckpoints[0].timestamp > _timestamp) return 0;
+
+        unchecked {
+            if (accountCheckpoints[nCheckpoints - 1].timestamp <= _timestamp) return accountCheckpoints[nCheckpoints - 1].votes;
+
+            uint256 high = nCheckpoints - 1;
+
+            uint256 low;
+
+            uint256 avg;
+
+            Checkpoint memory tempCP;
+
+            while (high > low) {
+                avg = (low & high) + (low ^ high) / 2;
+
+                tempCP = accountCheckpoints[avg];
+
+                if (tempCP.timestamp == _timestamp) {
+                    return tempCP.votes;
+                } else if (tempCP.timestamp < _timestamp) {
+                    low = avg;
+                } else {
+                    high = avg - 1;
+                }
+            }
+
+            return accountCheckpoints[low].votes;
+        }
+    }
+}
