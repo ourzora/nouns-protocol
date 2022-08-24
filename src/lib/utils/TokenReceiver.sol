28100bd21db8e4398b369452c6cc9c8d95ef9498
Rohan Kulkarni <rohan@zora.co>
2022-08-24 01:21:16 -0400

    chore: update pure utils

--- src/lib/utils/TokenReceiver.sol
+++ src/lib/utils/TokenReceiver.sol
@@ -1,6 +1,7 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.15;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+/// @notice Modified from OpenZeppelin Contracts v4.7.3 (token/ERC721/utils/ERC721Holder.sol)
 abstract contract ERC721TokenReceiver {
     function onERC721Received(
         address,
@@ -8,10 +9,11 @@ abstract contract ERC721TokenReceiver {
         uint256,
         bytes calldata
     ) external virtual returns (bytes4) {
-        return ERC721TokenReceiver.onERC721Received.selector;
+        return this.onERC721Received.selector;
     }
 }
 
+/// @notice Modified from OpenZeppelin Contracts v4.7.3 (token/ERC1155/utils/ERC1155Holder.sol)
 abstract contract ERC1155TokenReceiver {
     function onERC1155Received(
         address,
@@ -20,7 +22,7 @@ abstract contract ERC1155TokenReceiver {
         uint256,
         bytes calldata
     ) external virtual returns (bytes4) {
-        return ERC1155TokenReceiver.onERC1155Received.selector;
+        return this.onERC1155Received.selector;
     }
 
     function onERC1155BatchReceived(
@@ -30,6 +32,6 @@ abstract contract ERC1155TokenReceiver {
         uint256[] calldata,
         bytes calldata
     ) external virtual returns (bytes4) {
-        return ERC1155TokenReceiver.onERC1155BatchReceived.selector;
+        return this.onERC1155BatchReceived.selector;
     }
 }
