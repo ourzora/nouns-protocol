e2dd7f9762a116b48fb4ce3fb5e00981b8b499e0
Rohan Kulkarni <rohan@zora.co>
2022-08-29 12:08:53 -0400

    test: add mocks

---
+++ test/utils/mocks/MockERC1155.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.15;
+
+import { ERC1155 } from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+contract MockERC1155 is ERC1155 {
+    constructor() ERC1155("") {}
+
+    function mint(
+        address _to,
+        uint256 _tokenId,
+        uint256 _amount
+    ) public {
+        _mint(_to, _tokenId, _amount, "");
+    }
+
+    function mintBatch(
+        address _to,
+        uint256[] memory _tokenIds,
+        uint256[] memory _amounts
+    ) public {
+        _mintBatch(_to, _tokenIds, _amounts, "");
+    }
+}
