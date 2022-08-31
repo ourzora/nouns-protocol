7967c60148c9e821a7cfcb995603b41963e5b243
Rohan Kulkarni <rohan@zora.co>
2022-08-31 12:03:32 -0400

    docs: update metadata interfaces

--- src/token/metadata/interfaces/IPropertyIPFSMetadataRenderer.sol
+++ src/token/metadata/interfaces/IPropertyIPFSMetadataRenderer.sol
@@ -1,41 +1,45 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.15;
 
-import {MetadataRendererTypesV1} from "./types/MetadataRendererTypesV1.sol";
-import {INounsMetadata} from "./INounsMetadata.sol";
+import { MetadataRendererTypesV1 } from "../types/MetadataRendererTypesV1.sol";
+import { IBaseMetadata } from "./IBaseMetadata.sol";
 
+/// @title IPropertyIPFSMetadataRenderer
+/// @author Iain Nash & Rohan Kulkarni
+/// @notice The external PropertyIPFSMetadataRenderer
+interface IPropertyIPFSMetadataRenderer is IBaseMetadata, MetadataRendererTypesV1 {
+    ///                                                          ///
+    ///                            EVENTS                        ///
+    ///                                                          ///
 
-interface IPropertyIPFSMetadataRenderer is MetadataRendererTypesV1, INounsMetadata {
-    ///                                                          ///
-    ///                                                          ///
-    ///                                                          ///
-
+    /// @notice Emitted when a property is added
     event PropertyAdded(uint256 id, string name);
 
+    /// @notice Emitted when an item is added
     event ItemAdded(uint256 propertyId, uint256 index);
 
+    /// @notice Emitted when the contract image is updated
     event ContractImageUpdated(string prevImage, string newImage);
 
+    /// @notice Emitted when the renderer base is updated
     event RendererBaseUpdated(string prevRendererBase, string newRendererBase);
 
+    /// @notice Emitted when the collection description is updated
+    event DescriptionUpdated(string prevDescription, string newDescription);
+
     ///                                                          ///
-    ///                                                          ///
+    ///                            ERRORS                        ///
     ///                                                          ///
 
+    /// @dev
     error ONLY_TOKEN();
 
+    /// @dev
     error TOKEN_NOT_MINTED(uint256 tokenId);
 
     ///                                                          ///
+    ///                           FUNCTIONS                      ///
     ///                                                          ///
-    ///                                                          ///
-
-    function initialize(
-        bytes calldata initStrings,
-        address token,
-        address founders,
-        address treasury
-    ) external;
 
     function addProperties(
         string[] calldata names,
@@ -43,23 +47,19 @@ interface IPropertyIPFSMetadataRenderer is MetadataRendererTypesV1, INounsMetada
         IPFSGroup calldata ipfsGroup
     ) external;
 
-    function updateContractImage(string memory newContractImage) external;
-
-    function updateRendererBase(string memory newRendererBase) external;
-
     function propertiesCount() external view returns (uint256);
 
     function itemsCount(uint256 propertyId) external view returns (uint256);
 
     function getAttributes(uint256 tokenId) external view returns (bytes memory aryAttributes, bytes memory queryString);
 
-    function token() external view returns (address);
-
-    function treasury() external view returns (address);
-
     function contractImage() external view returns (string memory);
 
     function rendererBase() external view returns (string memory);
 
     function description() external view returns (string memory);
+
+    function updateContractImage(string memory newContractImage) external;
+
+    function updateRendererBase(string memory newRendererBase) external;
 }
