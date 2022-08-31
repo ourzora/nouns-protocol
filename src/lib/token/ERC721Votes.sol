d90f3c56775eb330e907f526492b52d6cee03774
Rohan Kulkarni <rohan@zora.co>
2022-08-31 12:07:36 -0400

    chore: update ERC721 and ERC721Votes

--- src/lib/token/ERC721Votes.sol
+++ src/lib/token/ERC721Votes.sol
@@ -1,59 +1,146 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.15;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
-import {EIP712} from "../utils/EIP712.sol";
-import {ERC721} from "../token/ERC721.sol";
+import { IERC721Votes } from "../interfaces/IERC721Votes.sol";
+import { ERC721 } from "../token/ERC721.sol";
+import { EIP712 } from "../utils/EIP712.sol";
 
-contract ERC721VotesTypesV1 {
-    struct Checkpoint {
-        uint64 timestamp;
-        uint192 votes;
-    }
-}
-
-contract ERC721VotesStorageV1 is ERC721VotesTypesV1 {
-    mapping(address => uint256) public numCheckpoints;
-
-    mapping(address => mapping(uint256 => Checkpoint)) public checkpoints;
-
-    mapping(address => address) internal delegation;
-}
-
-abstract contract ERC721Votes is EIP712, ERC721, ERC721VotesStorageV1 {
+/// @title ERC721Votes
+/// @author Rohan Kulkarni
+/// @notice Modified from OpenZeppelin Contracts v4.7.3 (token/ERC721/extensions/draft-ERC721Votes.sol)
+/// - Uses custom errors defined in IERC721Votes
+/// - Checkpoints use timestamps instead of block numbers
+/// - Tokens are self-delegated by default
+/// - Total number of votes isn't tracked, as that's the token supply itself (1 NFT = 1 Vote)
+abstract contract ERC721Votes is IERC721Votes, EIP712, ERC721 {
     ///                                                          ///
-    ///                           CONSTANTS                      ///
+    ///                          CONSTANTS                       ///
     ///                                                          ///
 
+    /// @dev The EIP-712 typehash to delegate with a signature
     bytes32 internal constant DELEGATION_TYPEHASH = keccak256("Delegation(address from,address to,uint256 nonce,uint256 deadline)");
 
     ///                                                          ///
-    ///                            EVENTS                        ///
+    ///                           STORAGE                        ///
     ///                                                          ///
 
-    event DelegateChanged(address indexed delegator, address indexed from, address indexed to);
+    /// @notice The delegate for an account
+    /// @notice Account => Delegate
+    mapping(address => address) internal delegation;
 
-    event DelegateVotesChanged(address indexed delegate, uint256 prevVotes, uint256 newVotes);
+    /// @notice The number of checkpoints for an account
+    /// @dev Account => Num Checkpoints
+    mapping(address => uint256) internal numCheckpoints;
+
+    /// @notice The checkpoint for an account
+    /// @dev Account => Checkpoint Id => Checkpoint
+    mapping(address => mapping(uint256 => Checkpoint)) internal checkpoints;
 
     ///                                                          ///
-    ///                            ERRORS                        ///
+    ///                        VOTING WEIGHT                     ///
     ///                                                          ///
 
-    error INVALID_TIMESTAMP();
+    /// @notice The current number of votes for an account
+    /// @param _account The account address
+    function getVotes(address _account) public view returns (uint256) {
+        // Get the account's number of checkpoints
+        uint256 nCheckpoints = numCheckpoints[_account];
 
-    ///                                                          ///
-    ///                           FUNCTIONS                      ///
-    ///                                                          ///
-
-    function delegates(address _user) external view returns (address) {
-        address current = delegation[_user];
-
-        return current == address(0) ? _user : current;
+        // Cannot underflow as `nCheckpoints` is ensured to be greater than 0 if reached
+        unchecked {
+            // Return the number of votes at the latest checkpoint if applicable
+            return nCheckpoints != 0 ? checkpoints[_account][nCheckpoints - 1].votes : 0;
+        }
     }
 
+    /// @notice The number of votes for an account at a past timestamp
+    /// @param _account The account address
+    /// @param _timestamp The past timestamp
+    function getPastVotes(address _account, uint256 _timestamp) public view returns (uint256) {
+        // Ensure the given timestamp is in the past
+        if (_timestamp >= block.timestamp) revert INVALID_TIMESTAMP();
+
+        // Get the account's number of checkpoints
+        uint256 nCheckpoints = numCheckpoints[_account];
+
+        // If there are none return 0
+        if (nCheckpoints == 0) return 0;
+
+        // Get the account's checkpoints
+        mapping(uint256 => Checkpoint) storage accountCheckpoints = checkpoints[_account];
+
+        unchecked {
+            // Get the latest checkpoint id
+            // Cannot underflow as `nCheckpoints` is ensured to be greater than 0
+            uint256 lastCheckpoint = nCheckpoints - 1;
+
+            // If the latest checkpoint has a valid timestamp, return its number of votes
+            if (accountCheckpoints[lastCheckpoint].timestamp <= _timestamp) return accountCheckpoints[lastCheckpoint].votes;
+
+            // On the flip, if the first checkpoint doesn't have a valid timestamp, return 0
+            if (accountCheckpoints[0].timestamp > _timestamp) return 0;
+
+            // Otherwise, find a checkpoint with a valid timestamp
+            // Use the latest id as the initial upper bound
+            uint256 high = lastCheckpoint;
+            uint256 low;
+            uint256 middle;
+
+            // Used to temporarily store a checkpoint
+            Checkpoint memory cp;
+
+            // While a valid checkpoint is to be found:
+            while (high > low) {
+                // Find the id of the middle checkpoint
+                middle = high - (high - low) / 2;
+
+                // Get the middle checkpoint
+                cp = accountCheckpoints[middle];
+
+                // If the timestamp is a match:
+                if (cp.timestamp == _timestamp) {
+                    // Return the voting weight
+                    return cp.votes;
+
+                    // Else if the timestamp is before the one looking for:
+                } else if (cp.timestamp < _timestamp) {
+                    // Update the lower bound
+                    low = middle;
+
+                    // Else update the upper bound
+                } else {
+                    high = middle - 1;
+                }
+            }
+
+            return accountCheckpoints[low].votes;
+        }
+    }
+
+    ///                                                          ///
+    ///                          DELEGATION                      ///
+    ///                                                          ///
+
+    /// @notice The delegate for an account
+    /// @param _account The account address
+    function delegates(address _account) external view returns (address) {
+        address current = delegation[_account];
+        return current == address(0) ? _account : current;
+    }
+
+    /// @notice Delegates votes to an account
+    /// @param _to The address delegating votes to
     function delegate(address _to) external {
         _delegate(msg.sender, _to);
     }
 
+    /// @notice Delegates votes from a signer to an account
+    /// @param _from The address delegating votes from
+    /// @param _to The address delegating votes to
+    /// @param _deadline The signature deadline
+    /// @param _v The 129th byte and chain id of the signature
+    /// @param _r The first 64 bytes of the signature
+    /// @param _s Bytes 64-128 of the signature
     function delegateBySig(
         address _from,
         address _to,
@@ -62,132 +149,122 @@ abstract contract ERC721Votes is EIP712, ERC721, ERC721VotesStorageV1 {
         bytes32 _r,
         bytes32 _s
     ) external {
+        // Ensure the signature has not expired
         if (block.timestamp > _deadline) revert EXPIRED_SIGNATURE();
 
+        // Used to store the message hash
         bytes32 digest;
 
+        // Cannot realistically overflow
         unchecked {
+            // Compute the signed message hash
             digest = keccak256(
                 abi.encodePacked("\x19\x01", DOMAIN_SEPARATOR(), keccak256(abi.encode(DELEGATION_TYPEHASH, _from, _to, nonces[_from]++, _deadline)))
             );
         }
 
+        // Recover the message signer
         address recoveredAddress = ecrecover(digest, _v, _r, _s);
 
+        // Ensure the recovered signer is the voter
         if (recoveredAddress == address(0) || recoveredAddress != _from) revert INVALID_SIGNER();
 
+        // Update the delegate
         _delegate(_from, _to);
     }
 
+    /// @dev Updates delegate addresses
+    /// @param _from The address delegating votes from
+    /// @param _to The address delegating votes to
     function _delegate(address _from, address _to) internal {
+        // Get the previous delegate
         address prevDelegate = delegation[_from];
 
+        // Store the new delegate
         delegation[_from] = _to;
 
         emit DelegateChanged(_from, prevDelegate, _to);
 
+        // Transfer voting weight from the previous delegate to the new delegate
         _moveDelegateVotes(prevDelegate, _to, balanceOf(_from));
     }
 
+    /// @dev Transfers voting weight
+    /// @param _from The address delegating votes from
+    /// @param _to The address delegating votes to
+    /// @param _amount The number of votes delegating
     function _moveDelegateVotes(
         address _from,
         address _to,
         uint256 _amount
-    ) private {
+    ) internal {
         unchecked {
+            // If voting weight is being transferred:
             if (_from != _to && _amount > 0) {
+                // If this isn't a mint:
                 if (_from != address(0)) {
+                    // Get the sender's number of checkpoints
                     uint256 nCheckpoints = numCheckpoints[_from]++;
 
+                    // Used to store the sender's previous voting weight
                     uint256 prevTotalVotes;
 
+                    // If this isn't the sender's first checkpoint: Get their previous voting weight
                     if (nCheckpoints != 0) prevTotalVotes = checkpoints[_from][nCheckpoints - 1].votes;
 
+                    // Update their voting weight
                     _writeCheckpoint(_from, nCheckpoints, prevTotalVotes, prevTotalVotes - _amount);
                 }
 
+                // If this isn't a burn:
                 if (_to != address(0)) {
+                    // Get the receiver's number of checkpoints
                     uint256 nCheckpoints = numCheckpoints[_to]++;
 
+                    // Used to store the receiver's previous voting weight
                     uint256 prevTotalVotes;
 
+                    // If this isn't the recipient's first checkpoint: Get their previous voting weight
                     if (nCheckpoints != 0) prevTotalVotes = checkpoints[_to][nCheckpoints - 1].votes;
 
+                    // Update their voting weight
                     _writeCheckpoint(_to, nCheckpoints, prevTotalVotes, prevTotalVotes + _amount);
                 }
             }
         }
     }
 
+    /// @dev Records a checkpoint
+    /// @param _account The account address
+    /// @param _id The checkpoint id
+    /// @param _prevTotalVotes The account's previous voting weight
+    /// @param _newTotalVotes The account's new voting weight
     function _writeCheckpoint(
-        address _user,
-        uint256 _index,
+        address _account,
+        uint256 _id,
         uint256 _prevTotalVotes,
         uint256 _newTotalVotes
     ) private {
-        Checkpoint storage checkpoint = checkpoints[_user][_index];
+        // Get the location to write the checkpoint
+        Checkpoint storage checkpoint = checkpoints[_account][_id];
 
+        // Store the new voting total and current time
         checkpoint.votes = uint192(_newTotalVotes);
         checkpoint.timestamp = uint64(block.timestamp);
 
-        emit DelegateVotesChanged(_user, _prevTotalVotes, _newTotalVotes);
+        emit DelegateVotesChanged(_account, _prevTotalVotes, _newTotalVotes);
     }
 
-    function getVotes(address _user) public view virtual returns (uint256) {
-        uint256 nCheckpoints = numCheckpoints[_user];
-
-        unchecked {
-            return nCheckpoints != 0 ? checkpoints[_user][nCheckpoints - 1].votes : 0;
-        }
-    }
-
-    function getPastVotes(address _user, uint256 _timestamp) public view returns (uint256) {
-        if (_timestamp >= block.timestamp) revert INVALID_TIMESTAMP();
-
-        uint256 nCheckpoints = numCheckpoints[_user];
-
-        if (nCheckpoints == 0) return 0;
-
-        mapping(uint256 => Checkpoint) storage userCheckpoints = checkpoints[_user];
-
-        unchecked {
-            uint256 lastCheckpoint = nCheckpoints - 1;
-
-            if (userCheckpoints[lastCheckpoint].timestamp <= _timestamp) return userCheckpoints[lastCheckpoint].votes;
-
-            if (userCheckpoints[0].timestamp > _timestamp) return 0;
-
-            uint256 high = lastCheckpoint;
-
-            uint256 low;
-
-            uint256 avg;
-
-            Checkpoint memory cp;
-
-            while (high > low) {
-                avg = (low & high) + (low ^ high) / 2;
-
-                cp = userCheckpoints[avg];
-
-                if (cp.timestamp == _timestamp) {
-                    return cp.votes;
-                } else if (cp.timestamp < _timestamp) {
-                    low = avg;
-                } else {
-                    high = avg - 1;
-                }
-            }
-
-            return userCheckpoints[low].votes;
-        }
-    }
-
+    /// @dev Enables each NFT to equal 1 vote
+    /// @param _from The token sender
+    /// @param _to The token recipient
+    /// @param _tokenId The ERC-721 token id
     function _afterTokenTransfer(
         address _from,
         address _to,
         uint256 _tokenId
     ) internal override {
+        // Transfer 1 vote from the sender to the recipient
         _moveDelegateVotes(_from, _to, 1);
 
         super._afterTokenTransfer(_from, _to, _tokenId);
