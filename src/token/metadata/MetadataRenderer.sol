b3477f00bc6682282dbaec48982f30fc5922d5fb
Iain <iain@zora.co>
2022-10-07 17:28:02 -0400

    updating remappings and tests

--- src/token/metadata/MetadataRenderer.sol
+++ src/token/metadata/MetadataRenderer.sol
@@ -5,6 +5,8 @@ import { Base64 } from "@openzeppelin/contracts/utils/Base64.sol";
 import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
 import { LibUintToString } from "sol2string/contracts/LibUintToString.sol";
 import { UriEncode } from "sol-uriencode/src/UriEncode.sol";
+import { MetadataBuilder } from "micro-onchain-metadata-utils/MetadataBuilder.sol";
+import { MetadataJSONKeys } from "micro-onchain-metadata-utils/MetadataJSONKeys.sol";
 
 import { UUPS } from "../../lib/proxy/UUPS.sol";
 import { Ownable } from "../../lib/utils/Ownable.sol";
@@ -203,9 +205,9 @@ contract MetadataRenderer is IPropertyIPFSMetadataRenderer, UUPS, Ownable, Metad
 
     /// @notice The properties and query string for a generated token
     /// @param _tokenId The ERC-721 token id
-    function getAttributes(uint256 _tokenId) public view returns (bytes memory aryAttributes, bytes memory queryString) {
+    function getAttributes(uint256 _tokenId) public view returns (string memory resultAttributes, string memory queryString) {
         // Get the token's query string
-        queryString = abi.encodePacked(
+        queryString = string.concat(
             "?contractAddress=",
             Strings.toHexString(uint256(uint160(address(this))), 20),
             "&tokenId=",
@@ -219,17 +221,15 @@ contract MetadataRenderer is IPropertyIPFSMetadataRenderer, UUPS, Ownable, Metad
         uint256 numProperties = tokenAttributes[0];
 
         // Ensure the given token was minted
-        if (numProperties == 0) revert TOKEN_NOT_MINTED(_tokenId);
+        if (numProperties == 0) {
+            revert TOKEN_NOT_MINTED(_tokenId);
+        }
+
+        MetadataBuilder.JSONItem[] memory arrayAttributesItems = new MetadataBuilder.JSONItem[](numProperties);
 
         unchecked {
-            // Cache the index of the last property
-            uint256 lastProperty = numProperties - 1;
-
             // For each of the token's properties:
             for (uint256 i = 0; i < numProperties; ++i) {
-                // Check if this is the last property
-                bool isLast = i == lastProperty;
-
                 // Get a copy of the property
                 Property memory property = properties[i];
 
@@ -240,10 +240,15 @@ contract MetadataRenderer is IPropertyIPFSMetadataRenderer, UUPS, Ownable, Metad
                 Item memory item = property.items[attribute];
 
                 // Store the encoded attributes and query string
-                aryAttributes = abi.encodePacked(aryAttributes, '"', property.name, '": "', item.name, '"', isLast ? "" : ",");
-                queryString = abi.encodePacked(queryString, "&images=", _getItemImage(item, property.name));
+                arrayAttributesItems[i].key = property.name;
+                arrayAttributesItems[i].value = item.name;
+                arrayAttributesItems[i].quote = true;
+
+                queryString = string.concat(queryString, "&images=", _getItemImage(item, property.name));
             }
         }
+
+        resultAttributes = MetadataBuilder.generateJSON(arrayAttributesItems);
     }
 
     /// @dev Generates a psuedo-random seed for a token id
@@ -267,46 +272,28 @@ contract MetadataRenderer is IPropertyIPFSMetadataRenderer, UUPS, Ownable, Metad
 
     /// @notice The contract URI
     function contractURI() external view returns (string memory) {
-        return
-            _encodeAsJson(
-                abi.encodePacked(
-                    '{"name": "',
-                    settings.name,
-                    '", "description": "',
-                    settings.description,
-                    '", "image": "',
-                    settings.contractImage,
-                    '"}'
-                )
-            );
+        MetadataBuilder.JSONItem[] memory items = new MetadataBuilder.JSONItem[](3);
+
+        items[0] = MetadataBuilder.JSONItem({ key: MetadataJSONKeys.keyName, value: settings.name, quote: true });
+        items[1] = MetadataBuilder.JSONItem({ key: MetadataJSONKeys.keyDescription, value: settings.description, quote: true });
+        items[2] = MetadataBuilder.JSONItem({ key: MetadataJSONKeys.keyImage, value: settings.contractImage, quote: true });
+
+        return MetadataBuilder.generateEncodedJSON(items);
     }
 
     /// @notice The token URI
     /// @param _tokenId The ERC-721 token id
     function tokenURI(uint256 _tokenId) external view returns (string memory) {
-        (bytes memory aryAttributes, bytes memory queryString) = getAttributes(_tokenId);
-        return
-            _encodeAsJson(
-                abi.encodePacked(
-                    '{"name": "',
-                    settings.name,
-                    " #",
-                    LibUintToString.toString(_tokenId),
-                    '", "description": "',
-                    settings.description,
-                    '", "image": "',
-                    settings.rendererBase,
-                    queryString,
-                    '", "properties": {',
-                    aryAttributes,
-                    "}}"
-                )
-            );
-    }
+        (string memory attributes, string memory queryString) = getAttributes(_tokenId);
 
-    /// @dev Encodes data to JSON
-    function _encodeAsJson(bytes memory _jsonBlob) private pure returns (string memory) {
-        return string(abi.encodePacked("data:application/json;base64,", Base64.encode(_jsonBlob)));
+        MetadataBuilder.JSONItem[] memory items = new MetadataBuilder.JSONItem[](4);
+
+        items[0] = MetadataBuilder.JSONItem({ key: MetadataJSONKeys.keyName, value: string.concat(settings.name, " #", Strings.toString(_tokenId)), quote: true });
+        items[1] = MetadataBuilder.JSONItem({ key: MetadataJSONKeys.keyDescription, value: settings.description, quote: true });
+        items[2] = MetadataBuilder.JSONItem({ key: MetadataJSONKeys.keyImage, value: string.concat(settings.rendererBase, queryString), quote: true });
+        items[3] = MetadataBuilder.JSONItem({ key: MetadataJSONKeys.keyProperties, value: attributes, quote: false });
+
+        return MetadataBuilder.generateEncodedJSON(items);
     }
 
     ///                                                          ///
