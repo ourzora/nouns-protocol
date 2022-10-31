ed33b6f3a4044d23967d22afc84d1421406b5ee9
Iain <iain@zora.co>
2022-10-31 17:59:36 -0400

    update metadata renderer test

--- test/MetadataRenderer.t.sol
+++ test/MetadataRenderer.t.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.16;
 
 import { NounsBuilderTest } from "./utils/NounsBuilderTest.sol";
 import { MetadataRendererTypesV1 } from "../src/token/metadata/types/MetadataRendererTypesV1.sol";
+import { MetadataRendererTypesV2 } from "../src/token/metadata/types/MetadataRendererTypesV2.sol";
 
 contract MetadataRendererTest is NounsBuilderTest, MetadataRendererTypesV1 {
     function setUp() public virtual override {
@@ -131,6 +132,60 @@ contract MetadataRendererTest is NounsBuilderTest, MetadataRendererTypesV1 {
         assertEq(metadataRenderer.description(), "new description");
         assertEq(metadataRenderer.projectURI(), "https://nouns.build/about");
 
+    }
+
+    function test_AddAdditionalProperties() public {
+        string[] memory names = new string[](1);
+        names[0] = "mock-property";
+
+        ItemParam[] memory items = new ItemParam[](1);
+        items[0].propertyId = 0;
+        items[0].name = "mock-item";
+        items[0].isNewProperty = true;
+
+        IPFSGroup memory ipfsGroup = IPFSGroup({ baseUri: "https://nouns.build/api/test/", extension: ".json" });
+
+        vm.prank(founder);
+        metadataRenderer.addProperties(names, items, ipfsGroup);
+
+        vm.prank(address(auction));
+        token.mint();
+
+
+        MetadataRendererTypesV2.AdditionalTokenProperty[] memory additionalTokenProperties = new MetadataRendererTypesV2.AdditionalTokenProperty[](2);
+        additionalTokenProperties[0] = MetadataRendererTypesV2.AdditionalTokenProperty({
+            key: "testing",
+            value: "HELLO",
+            quote: true
+        });
+        additionalTokenProperties[1] = MetadataRendererTypesV2.AdditionalTokenProperty({
+            key: "participationAgreement",
+            value: "This is a JSON quoted participation agreement.",
+            quote: true
+        });
+        vm.prank(founder);
+        metadataRenderer.setAdditionalTokenProperties(additionalTokenProperties);
+
+        /**
+            Token URI additional properties result:
+
+            {
+                "name": "Mock Token #0",
+                "description": "This is a mock token",
+                "image": "http://localhost:5000/render?contractAddress=0xa37a694f029389d5167808761c1b62fcef775288&tokenId=0&images=https%3a%2f%2fnouns.build%2fapi%2ftest%2fmock-property%2fmock-item.json",
+                "properties": {
+                    "mock-property": "mock-item"
+                },
+                "testing": "HELLO",
+                "participationAgreement": "This is a JSON quoted participation agreement."
+            }
+        
+        */
+
+        assertEq(
+            token.tokenURI(0),
+            "data:application/json;base64,eyJuYW1lIjogIk1vY2sgVG9rZW4gIzAiLCJkZXNjcmlwdGlvbiI6ICJUaGlzIGlzIGEgbW9jayB0b2tlbiIsImltYWdlIjogImh0dHA6Ly9sb2NhbGhvc3Q6NTAwMC9yZW5kZXI/Y29udHJhY3RBZGRyZXNzPTB4YTM3YTY5NGYwMjkzODlkNTE2NzgwODc2MWMxYjYyZmNlZjc3NTI4OCZ0b2tlbklkPTAmaW1hZ2VzPWh0dHBzJTNhJTJmJTJmbm91bnMuYnVpbGQlMmZhcGklMmZ0ZXN0JTJmbW9jay1wcm9wZXJ0eSUyZm1vY2staXRlbS5qc29uIiwicHJvcGVydGllcyI6IHsibW9jay1wcm9wZXJ0eSI6ICJtb2NrLWl0ZW0ifSwidGVzdGluZyI6ICJIRUxMTyIsInBhcnRpY2lwYXRpb25BZ3JlZW1lbnQiOiAiVGhpcyBpcyBhIEpTT04gcXVvdGVkIHBhcnRpY2lwYXRpb24gYWdyZWVtZW50LiJ9"
+        );
     }
 
     function test_TokenURI() public {
