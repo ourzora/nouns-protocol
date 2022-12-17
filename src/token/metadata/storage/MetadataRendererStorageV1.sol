0973df026b14c7ccc57319d47c0f4a9bfa9474c8
Iain Nash <me@iain.in>
2022-12-17 01:24:45 -0500

    Add getters for metadata renderer information (#87)
    * add getters for metadata renderer information
    
    * fix tests

--- src/token/metadata/storage/MetadataRendererStorageV1.sol
+++ src/token/metadata/storage/MetadataRendererStorageV1.sol
@@ -8,14 +8,16 @@ import { MetadataRendererTypesV1 } from "../types/MetadataRendererTypesV1.sol";
 /// @notice The Metadata Renderer storage contract
 contract MetadataRendererStorageV1 is MetadataRendererTypesV1 {
     /// @notice The metadata renderer settings
-    Settings internal settings;
+    Settings public settings;
 
     /// @notice The properties chosen from upon generation
-    Property[] internal properties;
+    Property[] public properties;
 
     /// @notice The IPFS data of all property items
-    IPFSGroup[] internal ipfsData;
+    IPFSGroup[] public ipfsData;
 
-    /// @notice The attributes generated for a token
-    mapping(uint256 => uint16[16]) internal attributes;
+    /// @notice The attributes generated for a token - mapping of tokenID uint16 array
+    /// @dev Array of size 16 1st element [0] used for number of attributes chosen, next N elements for those selections
+    /// @dev token ID
+    mapping(uint256 => uint16[16]) public attributes;
 }
