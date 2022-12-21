d673cc05178d89627d18ebcad851a8bf89943fe8
jgeary <36774175+jgeary@users.noreply.github.com>
2022-12-21 14:47:04 -0500

    Update Token.sol

--- src/token/Token.sol
+++ src/token/Token.sol
@@ -18,7 +18,6 @@ import { VersionedContract } from "../VersionedContract.sol";
 /// @author Rohan Kulkarni
 /// @custom:repo github.com/ourzora/nouns-protocol 
 /// @notice A DAO's ERC-721 governance token
-
 contract Token is IToken, VersionedContract, UUPS, Ownable, ReentrancyGuard, ERC721Votes, TokenStorageV1, TokenStorageV2 {
     ///                                                          ///
     ///                         IMMUTABLES                       ///
