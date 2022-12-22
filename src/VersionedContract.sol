d31a08fa46259f3a6f70b05ab92fbab05dba6997
Tyson Battistella <tysonbattistella@gmail.com>
2022-12-21 16:51:44 -0800

    T/version1 1 release (#95)
    * [chore] Bump version to 1.1
    
    * [feat] Deploy Version 1.1 to Goerli
    
    * [feat] Deploy Version 1.1 to Mainnet

--- src/VersionedContract.sol
+++ src/VersionedContract.sol
@@ -3,6 +3,6 @@ pragma solidity 0.8.16;
 
 abstract contract VersionedContract {
     function contractVersion() external pure returns (string memory) {
-        return "1.0.0";
+        return "1.1.0";
     }
 }
